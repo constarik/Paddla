38835b1c1efbe424909255b9850b469791ea76ec
Constantin Razinsky <constr@gmail.com>
2026-04-12 14:11:21 +0300

    v9.0.4: fix Bet undefined — use numBalls*betPerBall

---
+++ find18.bat
@@ -0,0 +1,3 @@
+findstr /n "totalBet" C:\Users\const\ClaudeLab\PADDLA\client\index.html
+findstr /n "currentGameSeed" C:\Users\const\ClaudeLab\PADDLA\client\index.html
+findstr /n "gameEnd" C:\Users\const\ClaudeLab\PADDLA\client\index.html
---
+++ find19.bat
@@ -0,0 +1,2 @@
+findstr /n "vClientSeed" C:\Users\const\ClaudeLab\PADDLA\client\index.html
+findstr /n "vGameId" C:\Users\const\ClaudeLab\PADDLA\client\index.html
