aca83445335418de41e6f1ad6be7a67c993d17c9
Constantin Razinsky <constr@gmail.com>
2026-04-12 15:01:10 +0300

    v9.0.7: remove duplicate version dim green unify badge

---
+++ find24.bat
@@ -0,0 +1,3 @@
+findstr /n "version-badge" C:\Users\const\ClaudeLab\PADDLA\client\index.html
+findstr /n "pf-badge" C:\Users\const\ClaudeLab\PADDLA\client\index.html
+findstr /n "00ff66" C:\Users\const\ClaudeLab\PADDLA\client\index.html
