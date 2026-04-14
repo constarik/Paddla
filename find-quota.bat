ca6c746b4d8b168241e649fbb693e77522ce8e9e
Constantin Razinsky <constr@gmail.com>
2026-04-14 10:29:01 +0300

    v9.0.9 u3: quota only for server sim local is unlimited

---
+++ find-quota.bat
@@ -0,0 +1,3 @@
+findstr /n "quota" C:\Users\const\ClaudeLab\PADDLA\client\sim.html
+findstr /n "simBalls" C:\Users\const\ClaudeLab\PADDLA\client\sim.html
+findstr /n "SIM_BALLS" C:\Users\const\ClaudeLab\PADDLA\client\sim.html
